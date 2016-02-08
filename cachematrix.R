31247d422b0b3c0a5f35311dbce2f2a8fea54253
Patrick Gomes <patrickgomes@hotmail.com>
2016-02-07 22:33:49 -0600

    First commit of Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions act in conjunction to find the inverse of
+## an inputted (square) matrix. 
+## While the 'Solve' fucntion in R accomplishes this handily enough,
+## it can have large computational overheads in doing so with 
+## large matrices. The code below relies on the lexcical scoping
+## feature of R, using  the <<- operator which
+## can be used to assign a value to an object in an environment 
+## that is different from the current environment.
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    if(!(nrow(i)==ncol(i))) {
+        message("The Matrix isn't a Square! Exiting...")
+        stop()
+    } else { ##go ahead and find the inverse
+        data <- x$get()
+        i <- Solve(data, ...)
+        x$setinverse(i)
+        i
+    }
 }
